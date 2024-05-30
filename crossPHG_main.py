eb556199018958bea6ae47dfeff8186e1ae14827
Leo Chen <yuliangchen@ucsb.edu>
2024-05-30 00:27:53 -0700

    eval bug

--- crossPHG_main.py
+++ crossPHG_main.py
@@ -70,8 +70,8 @@ def evaluate(args,data_loader, model, device):
         loss += criterion(pred, labels)
         acc1 += accuracy(pred, labels, topk=(1,))[0]
 
-    epoch_loss.append(loss.cpu().float().numpy())
-    epoch_acc.append(acc1.cpu().float().numpy())
+        epoch_loss.append(loss.cpu().float().numpy())
+        epoch_acc.append(acc1.cpu().float().numpy())
 
     epoch_loss = np.mean(epoch_loss)
     epoch_acc = np.mean(epoch_acc)
